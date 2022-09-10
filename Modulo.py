61428dfc76144e3bb4ea15d95f92e3afb469a2d8
Valentino Amato <valentinoamatoroberts@gmail.com>
2022-09-09 21:07:21 -0300

    Added Module
    Added a test module (Modulo.py)

---
+++ Modulo.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env/python3                          #Define dónde se encuentra el intérprete.Es necesario para la correcta ejecicion del programa en sistemas operativos basados en unix
+#Creamos el docstring del Modulo y en el describimos su funcionamiento:
+"""Este modulo realiza                            
+operaciones aritmeticas
+(suma,resta,multiplicacion y division)"""
+__datos__= "hola"
+variable_x = "hola"
+def suma(a,b):                                    #Creamos
+    """Esta funcion suma los valores a y b"""
+    return a + b
+def resta(a,b):                                   #Las
+    """Esta funcion suma los valores a y b"""
+    return a - b
+def multiplicacion(a,b):                          #Funciones
+    """Esta funcion suma los valores a y b"""
+    return a * b
+def division(a,b):                                 #Con sus respectiva informacion y codigo
+    """Esta funcion suma los valores a y b"""
+    return a / b
